59d77097b5d176aafcf6a11be41361dc17841d62
Gernot Vormayr <notti@fet.at>
2010-04-27 17:39:59 +0200

    Processor design with Registers

--- vhdl/inbuf/inbuf.vhd
+++ vhdl/inbuf/inbuf.vhd
@@ -21,9 +21,6 @@ library misc;
 	use misc.procedures.all;
 
 entity inbuf is
-generic(
-        CLK_FREQ            : integer := 100000000
-);
 port(
 -- signals for gtx transciever
         refclk              : in  std_logic;
@@ -33,51 +30,55 @@ port(
         txn                 : out std_logic_vector(3 downto 0);
         txp                 : out std_logic_vector(3 downto 0);
 
--- control signals
-        input_select        : in std_logic_vector(1 downto 0);
+-- control signals reciever
         polarity            : in std_logic_vector(2 downto 0);
         descramble          : in std_logic_vector(2 downto 0);
-
-        rxeqmix             : in std_logic_vector(5 downto 0);
+        rxeqmix             : in t_cfg_array(2 downto 0);
+        data_valid          : out std_logic_vector(2 downto 0);
         enable              : in std_logic_vector(2 downto 0);
 
--- status signals
-        aligned             : out std_logic_vector(2 downto 0)
+-- control signals inbuf
+--        start_recv          : in std_logic;
+--        start_conv          : in std_logic;
+--        size                : in std_logic_vector(15 downto 0);
+        input_select        : in std_logic_vector(1 downto 0);
+
+--        cpu_req             : in std_logic;
+--        cpu_ack             : out std_logic;
+--        cpu_addr            : in std_logic_vector(15 downto 0);
+--        cpu_data2mem        : in std_logic_vector(31 downto 0);
+--        cpu_mem2data        : out std_logic_vector(31 downto 0);
+--        cpu_we              : in std_logic;
+--        cpu_re              : in std_logic;
+--		cpu_clk				: in std_logic;
+
+		data_clk		    : out std_logic
 );
 end inbuf;
 
 architecture Structural of inbuf is
         signal clk_i        : std_logic;
         signal data_i       : t_data_array(2 downto 0);
-        signal data_valid_i : std_logic_vector(2 downto 0);
-        signal rst_i        : std_logic;
-
--- FIXME!
-        signal rst_tmp              : std_logic; -- FIXME!
-        signal input_select_tmp       : std_logic_vector(1 downto 0);
-        signal polarity_tmp           : std_logic_vector(2 downto 0);
-        signal descramble_tmp         : std_logic_vector(2 downto 0);
-        signal rxeqmix_tmp            : t_cfg_array(2 downto 0);
-        signal enable_tmp             : std_logic_vector(2 downto 0);
 begin
 
 reciever_i: entity inbuf.reciever
 port map(
         refclk              => refclk,
-        rst                 => rst_tmp, -- FIXME!
+        rst                 => rst,
         rxn                 => rxn,
         rxp                 => rxp,
         txn                 => txn,
         txp                 => txp,
         clk                 => clk_i,
         data                => data_i,
-        data_valid          => data_valid_i,
-        rst_out             => rst_i,
-        polarity            => polarity_tmp,
-        descramble          => descramble_tmp,
-        rxeqmix             =>  rxeqmix_tmp,
-        enable              => enable_tmp
+        polarity            => polarity,
+        descramble          => descramble,
+        rxeqmix             => rxeqmix,
+        data_valid          => data_valid,
+        enable              => enable
 );
+
+data_clk <= clk_i;
 
 end Structural;
 
