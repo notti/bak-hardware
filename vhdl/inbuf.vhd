9e67a6f17a1aa38548ee6a526853f37df3db813b
Gernot Vormayr <gvormayr@gmail.com>
2011-10-04 15:20:23 +0200

    unfinished rewrite of inbuf

--- vhdl/inbuf.vhd
+++ vhdl/inbuf.vhd
@@ -45,29 +45,14 @@ port(
     arm                 : in  std_logic;
     trigger             : in  std_logic;
     avg_done            : out std_logic;
-    frame_clk           : out std_logic;
+    frame_index         : out std_logic_vector(15 downto 0);
     rst                 : in  std_logic;
-    locked              : out std_logic;
-
--- data
-    mem_req             : in  std_logic;
-    mem_ack             : out std_logic;
-    fpga2bus_error      : out std_logic;
-    fpga2bus_wrack      : out std_logic;
-    fpga2bus_rdack      : out std_logic;
-    fpga2bus_data       : out std_logic_vector(31 downto 0);
-    bus2fpga_rnw        : in  std_logic;
-    bus2fpga_cs         : in  std_logic_vector(3 downto 0);
-    bus2fpga_be         : in  std_logic_vector(3 downto 0);
-    bus2fpga_data       : in  std_logic_vector(31 downto 0);
-    bus2fpga_addr       : in  std_logic_vector(15 downto 0);
-    bus2fpga_reset      : in  std_logic;
-    bus2fpga_clk        : in  std_logic
+    frame_lock          : out std_logic;
 );
 end inbuf;
 
 architecture Structural of inbuf is
-    signal clk_i               : std_logic;
+    signal data_clk               : std_logic;
     signal rst_out_i           : std_logic;
     signal rec_data_i          : t_data_array(2 downto 0);
     signal data_i              : t_data;
@@ -79,58 +64,51 @@ architecture Structural of inbuf is
     signal mem_req_synced      : std_logic;
     signal arm_synced          : std_logic;
     signal arm_synced_dly      : std_logic;
-    signal arm_toggle           : std_logic;
+    signal arm_toggle          : std_logic;
     signal inbuf_rst           : std_logic;
     signal rst_synced          : std_logic;
     signal rst_synced_dly      : std_logic;
-    signal rst_toggle           : std_logic;
+    signal rst_toggle          : std_logic;
     signal select_synced       : std_logic;
     signal select_synced_dly   : std_logic;
-    signal select_toggle        : std_logic;
-    signal depth_synced       : std_logic;
-    signal depth_synced_dly   : std_logic;
+    signal select_toggle       : std_logic;
+    signal depth_synced        : std_logic;
+    signal depth_synced_dly    : std_logic;
     signal depth_toggle        : std_logic;
-    signal width_synced       : std_logic;
-    signal width_synced_dly   : std_logic;
+    signal width_synced        : std_logic;
+    signal width_synced_dly    : std_logic;
     signal width_toggle        : std_logic;
     signal select_r            : std_logic_vector(1 downto 0);
     signal width_r             : std_logic_vector(1 downto 0);
-    signal depth_1             : std_logic_vector(15 downto 0);
-    signal mem_read_cycle      : std_logic_vector(3 downto 0);
-    signal mem_write_cycle     : std_logic_vector(4 downto 0);
-    signal mem_low_word_r      : std_logic_vector(15 downto 0);
-    signal mem_high_word_r     : std_logic_vector(15 downto 0);
+    signal depth_r             : std_logic_vector(15 downto 0);
     signal inbuf_addr_data     : std_logic_vector(15 downto 0);
     signal inbuf_we            : std_logic;
     signal inbuf_data_out      : std_logic_vector(15 downto 0);
     signal inbuf_data_in       : std_logic_vector(15 downto 0);
 begin
-
-
-
     sync_gen: for i in 0 to 2 generate
         sync_enable_i: entity work.flag
         port map(
             flag_in      => rec_enable(i),
             flag_out     => rec_enable_synced(i),
-            clk          => clk_i
+            clk          => data_clk
         );
         sync_polarity_i: entity work.flag
         port map(
             flag_in      => rec_polarity(i),
             flag_out     => rec_polarity_synced(i),
-            clk          => clk_i
+            clk          => data_clk
         );
     end generate;
     sync_arm_i: entity work.flag
     port map(
         flag_in      => arm,
         flag_out     => arm_synced,
-        clk          => clk_i
+        clk          => data_clk
     );
-    arm_dly: process(clk_i)
+    arm_dly: process(data_clk)
     begin
-        if rising_edge(clk_i) then
+        if rising_edge(data_clk) then
             arm_synced_dly <= arm_synced;
         end if;
     end process;
@@ -139,11 +117,11 @@ begin
     port map(
         flag_in      => rst,
         flag_out     => rst_synced,
-        clk          => clk_i
+        clk          => data_clk
     );
-    rst_dly: process(clk_i)
+    rst_dly: process(data_clk)
     begin
-        if rising_edge(clk_i) then
+        if rising_edge(data_clk) then
             rst_synced_dly <= rst_synced;
         end if;
     end process;
@@ -152,7 +130,7 @@ begin
     port map(
         flag_in      => mem_req,
         flag_out     => mem_req_synced,
-        clk          => clk_i
+        clk          => data_clk
     );
 
 
@@ -165,7 +143,7 @@ begin
         txn                 => txn,
         txp                 => txp,
         pll_locked          => pll_locked,
-        clk                 => clk_i,
+        clk                 => data_clk,
         rst_out             => rst_out_i,
         data                => rec_data_i,
         polarity            => rec_polarity_synced,
@@ -179,20 +157,20 @@ begin
     port map(
         flag_in      => rec_input_select_req,
         flag_out     => select_synced,
-        clk          => clk_i
+        clk          => data_clk
     );
-    select_dly: process(clk_i)
+    select_dly: process(data_clk)
     begin
-        if rising_edge(clk_i) then
+        if rising_edge(data_clk) then
             select_synced_dly <= select_synced;
         end if;
     end process;
     select_toggle <= select_synced_dly xor select_synced;
-	select_r_p: process(clk_i, rst, select_toggle, rec_input_select)
+	select_r_p: process(data_clk, rst, select_toggle, rec_input_select)
     begin
         if rst = '1' then
             select_r <= (others => '0');
-		elsif rising_edge(clk_i) then
+		elsif rising_edge(data_clk) then
             if select_toggle = '1' then
                 select_r <= rec_input_select;
             end if;
@@ -202,9 +180,9 @@ begin
     rst_i <= or_many(rst_out_i & rst_toggle & select_toggle & depth_toggle & width_toggle);
     inbuf_rst <= or_many(rst_out_i & rst_toggle);
 
-	mux_process: process(clk_i, rec_data_i, select_r, rec_data_valid_i)
+	mux_process: process(data_clk, rec_data_i, select_r, rec_data_valid_i)
 	begin
-		if rising_edge(clk_i) then
+		if rising_edge(data_clk) then
 			case select_r is
 				when "00" => data_i <= rec_data_i(0); stream_valid_i <= rec_data_valid_i(0);
 				when "01" => data_i <= rec_data_i(1); stream_valid_i <= rec_data_valid_i(1);
@@ -218,22 +196,22 @@ begin
     port map(
         flag_in      => depth_req,
         flag_out     => depth_synced,
-        clk          => clk_i
+        clk          => data_clk
     );
-    depth_dly: process(clk_i)
+    depth_dly: process(data_clk)
     begin
-        if rising_edge(clk_i) then
+        if rising_edge(data_clk) then
             depth_synced_dly <= depth_synced;
         end if;
     end process;
     depth_toggle <= depth_synced_dly xor depth_synced;
-    depth_r_p: process(clk_i, rst, depth_toggle, depth)
+    depth_r_p: process(data_clk, rst, depth_toggle, depth)
     begin
         if rst = '1' then
-            depth_1 <= (others => '0');
-		elsif rising_edge(clk_i) then
+            depth_r <= (others => '0');
+		elsif rising_edge(data_clk) then
             if depth_toggle = '1' then
-                depth_1 <= depth - 1;
+                depth_r <= depth;
             end if;
         end if;
     end process;
@@ -241,126 +219,67 @@ begin
     port map(
         flag_in      => width_req,
         flag_out     => width_synced,
-        clk          => clk_i
+        clk          => data_clk
     );
-    width_dly: process(clk_i)
+    width_dly: process(data_clk)
     begin
-        if rising_edge(clk_i) then
+        if rising_edge(data_clk) then
             width_synced_dly <= width_synced;
         end if;
     end process;
     width_toggle <= width_synced_dly xor width_synced;
-    width_r_p: process(clk_i, rst, width_toggle, width)
+    width_r_p: process(data_clk, rst, width_toggle, width)
     begin
         if rst = '1' then
             width_r <= (others => '0');
-		elsif rising_edge(clk_i) then
+		elsif rising_edge(data_clk) then
             if width_toggle = '1' then
                 width_r <= width;
             end if;
         end if;
     end process;
-	fpga2bus_error <= '0';
 
-    --------------------------------------
-    -- cycle 0: read 0
-    -- cycle 1: read 1  data 0
-    -- cycle 2:         data 1
-    mem_read_cycle(0) <= '1' when bus2fpga_cs = "0001" and bus2fpga_rnw = '1' else '0';
-	mem_read_cycle_p: process(bus2fpga_clk, bus2fpga_reset, mem_read_cycle, bus2fpga_cs)
-	begin
-        if bus2fpga_reset = '1' or not (bus2fpga_cs = "0001") then
-            mem_read_cycle(3 downto 1) <= (others => '0');
-        elsif rising_edge(bus2fpga_clk) then
-            mem_read_cycle(3 downto 1) <= mem_read_cycle(2 downto 0);
-		end if;
-	end process mem_read_cycle_p;
+    --statemachine overall + auto
 
-    fpga2bus_rdack <= mem_read_cycle(3) when bus2fpga_cs = "0001" and bus2fpga_rnw = '1' else '0';
-
-    mem_low_word_p: process(bus2fpga_clk, mem_read_cycle, mem_write_cycle, inbuf_data_out)
-    begin
-        if rising_edge(bus2fpga_clk) then
-            if mem_read_cycle = "0011" or mem_write_cycle = "00011" then
-                mem_low_word_r <= inbuf_data_out;
-            end if;
-        end if;
-    end process mem_low_word_p;
-
-    inbuf_addr_data(15 downto 1) <= bus2fpga_addr(14 downto 0) when bus2fpga_cs = "0001" else
-                                    (others => '0');
-	inbuf_addr_data(0) <= '1' when bus2fpga_cs = "0001" and (mem_read_cycle = "0011" or mem_write_cycle = "00011" or mem_write_cycle = "01111") else
-					      '0';
-
-    fpga2bus_data_dly: process(bus2fpga_clk)
-    begin
-        if rising_edge(bus2fpga_clk) then
-	        fpga2bus_data <= inbuf_data_out & mem_low_word_r;
-        end if;
-    end process;
-    ---------------------------------------------------
-    -- cycle 0: read 0
-    -- cycle 1: read 1 data 0
-    -- cycle 2         data 1
-    -- cycle 3: wr 1
-    -- cycle 4: wr 0
-    mem_write_cycle(0) <= '1' when bus2fpga_cs = "0001" and bus2fpga_rnw = '0' else '0';
-	mem_write_cycle_p: process(bus2fpga_clk, bus2fpga_reset, mem_write_cycle, bus2fpga_cs)
-	begin
-        if bus2fpga_reset = '1' or not (bus2fpga_cs = "0001") then
-            mem_write_cycle(4 downto 1) <= (others => '0');
-        elsif rising_edge(bus2fpga_clk) then
-            mem_write_cycle(4 downto 1) <= mem_write_cycle(3 downto 0);
-		end if;
-	end process mem_write_cycle_p;
-    mem_high_word_p: process(bus2fpga_clk, mem_write_cycle, inbuf_data_out)
-    begin
-        if rising_edge(bus2fpga_clk) then
-            if mem_write_cycle = "00111" then
-                mem_high_word_r <= inbuf_data_out;
-            end if;
-        end if;
-    end process mem_high_word_p;
-
-	fpga2bus_wrack <= mem_write_cycle(4) when bus2fpga_cs = "0001" else '0';
-
-    inbuf_we <= mem_write_cycle(3) or mem_write_cycle(4) when bus2fpga_cs = "0001" else '0';
-    inbuf_data_in(7 downto 0)  <= bus2fpga_data(7 downto 0) when bus2fpga_be(0) = '1' and mem_write_cycle = "11111" else
-                                  bus2fpga_data(23 downto 16) when bus2fpga_be(2) = '1' and mem_write_cycle = "01111" else
-                                  mem_low_word_r(7 downto 0) when bus2fpga_be(0) = '0' and mem_write_cycle = "11111" else
-                                  mem_high_word_r(7 downto 0) when bus2fpga_be(2) = '0' and mem_write_cycle = "01111" else
-                                  (others => '0');
-    inbuf_data_in(15 downto 8) <= bus2fpga_data(15 downto 8) when bus2fpga_be(1) = '1' and mem_write_cycle = "11111" else
-                                  bus2fpga_data(31 downto 24) when bus2fpga_be(3) = '1' and mem_write_cycle = "01111" else
-                                  mem_low_word_r(15 downto 8) when bus2fpga_be(1) = '0' and mem_write_cycle = "11111" else
-                                  mem_high_word_r(15 downto 8) when bus2fpga_be(3) = '0' and mem_write_cycle = "01111" else
-                                  (others => '0');
+    --trigger
 
     average_mem_i: entity work.average_mem
     port map(
-        clk                     => clk_i,
+        clk                     => data_clk,
+        rst                     => rst_i, -- fixme
         width                   => width_r,
-        depth_1                 => depth_1,
-        arm                     => arm_toggle,
+        depth                   => depth_r,
+        trig                    => trigger, -- fixme - trigger generator
         done                    => avg_done,
-        trigger                 => trigger,
-        frame_clk               => frame_clk,
-        locked                  => locked,
-        rst                     => rst_i,
         data                    => data_i,
-        stream_valid            => stream_valid_i,
-        rst_arb                 => inbuf_rst,
-        read_req                => mem_req_synced,
-        read_ack                => mem_ack,
-        clk_data                => bus2fpga_clk,
-        addr                    => inbuf_addr_data,
-		we 						=> inbuf_we,
-        dout                    => inbuf_data_out,
-		din 				    => inbuf_data_in
+        memclk                  => --fixme
+        ext                     => --fixme
+        dina                    => --fixme
+        addra                   => --fixme
+        wea                     => --fixme
+        douta                   => --fixme
+        dinb                    => --fixme
+        addrb                   => --fixme
+        web                     => --fixme
+        doutb                   => --fixme
     );
 
+    wallclk_i : entity work.wallclk
+    port map(
+       clk                      => data_clk,
+       rst                      => rst_i, --fixme
+       n                        => depth_r,
+       wave_index               => --fixme
+       frame_clk                => --fixme
+       frame_trg                => --fixme
+       frame_index              => --fixme
+    );
+
+    --fft
+
+    --outbuf
+
     rec_data_valid <= rec_data_valid_i;
-    sample_clk <= clk_i;
 	rec_stream_valid <= stream_valid_i;
 
 end Structural;
---
+++ vhdl/wallclk.vhd
@@ -0,0 +1,62 @@
+----------------------------------------------------------
+-- Project			: 
+-- File				: wallclk.vhd
+-- Author			: Gernot Vormayr
+-- created			: July, 3rd 2009
+-- contents			: overlap add
+-----------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+library work;
+use work.all;
+
+entity wallclk is
+port(
+    clk        : in std_logic;
+    rst        : in std_logic;
+    n          : in std_logic_vector(15 downto 0);
+    wave_index : out std_logic_vector(3 downto 0);
+    frame_clk  : out std_logic;
+    frame_trg  : out std_logic;    
+    frame_index : out std_logic;
+);
+end wallclk;
+
+architecture Structural of wallclk is
+    signal cnt_wave: std_logic_vector(3 downto 0) := X"0";
+    signal cnt_frame: std_logic_vector(15 downto 0) := X"0000";
+    signal n_i : std_logic_vector(15 downto 0);
+begin
+    wave_index <= cnt_wave;
+    frame_index <= cnt_frame;
+    frame_trg <= '1' when cnt_frame = n_i else
+                 '0';
+    frame_clk <= '1' when cnt_frame = X"0000" else
+                 '0';
+
+    cnt_p: process(clk, rst)
+    begin
+        if clk = '1' and clk'event then
+            if rst = '1' then
+                cnt_wave <= X"0";
+                cnt_frame <= X"0000";
+                n_i <= n - 1;
+            else
+                if cnt_wave = X"9" then
+                    cnt_wave <= (others => '0');
+                else
+                    cnt_wave <= cnt_wave + 1;
+                end if;
+                if cnt_frame = n_i then
+                    cnt_frame <= (others => '0');
+                else
+                    cnt_frame <= cnt_frame + 1;
+                end if;
+            end if;
+        end if;
+    end process cnt_p;
+
+end Structural;
+
