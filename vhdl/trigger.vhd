1b7855f3b8c7fd804701da0a97844ef300df4dfe
Gernot Vormayr <gvormayr@gmail.com>
2011-10-19 15:51:35 +0200

    finished trigger

--- vhdl/trigger.vhd
+++ vhdl/trigger.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------
 -- Project			: 
--- File				: reciever.vhd
+-- File				: trigger.vhd
 -- Author			: Gernot Vormayr
 -- created			: July, 3rd 2009
 -- contents			: overlap add
@@ -12,7 +12,7 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 library work;
 use work.all;
 
-entity reciever is
+entity trigger is
 port(
     clk         : in  std_logic;
     rst         : in  std_logic;
@@ -23,10 +23,56 @@ port(
     arm         : in  std_logic;
     trig        : out std_logic;
 );
-end reciever;
+end trigger;
 
-architecture Structural of reciever is
+architecture Structural of trigger is
+    type trigger_state is (RESET, WAIT_TRG, TRIGD, INACTIVE, WAIT_FRM);
+    signal trg: std_logic;
+    signal state: trigger_state;
+
 begin
+    process(clk, rst)
+    begin
+        if clk'event and clk = '1' then
+            if rst = '1' then
+                state <= RESET;
+            else
+                case state is
+                    when RESET =>
+                        if arm = '1' then
+                            state <= WAIT_TRG;
+                        else
+                            state <= RESET;
+                        end if;
+                    when WAIT_TRG =>
+                        if trg = '1' then
+                            state <= TRIGD;
+                        else
+                            state <= WAIT_TRG;
+                        end if;
+                    when TRIGD =>
+                        state <= INACTIVE;
+                    when INACTIVE =>
+                        if arm = '1' then
+                            state <= WAIT_FRM;
+                        else
+                            state <= INACTIVE;
+                        end if;
+                    when WAIT_FRM =>
+                        if frame_trg = '1' then
+                            state <= TRIGD;
+                        else
+                            state <= WAIT_FRM;
+                        end if;
+                end case;
+            end if;
+        end if;
+    end process;
+
+    trg <= trigger_int when typ = '0' else
+           trigger_ext;
+    trig <= '1' when state = TRIGD else
+            '0';
 
 end Structural;
 
