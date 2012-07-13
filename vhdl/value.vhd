b250fd48a6eee9d7a32698e7a22a6357582605d7
Gernot Vormayr <gvormayr@gmail.com>
2012-07-14 00:40:12 +0200

    final version of value

--- vhdl/value.vhd
+++ vhdl/value.vhd
@@ -6,52 +6,33 @@ port(
     value_in  : in  std_logic_vector;
     value_out : out std_logic_vector(value_in'range);
     value_wr  : in  std_logic;
+    busy      : out std_logic;
     clk_from  : in  std_logic;
     clk_to    : in  std_logic
 );
 end value;
 
 architecture Structural of value is
-    signal value_wr_r       : std_logic;
-    signal value_wr_one     : std_logic;
-    signal value_in_reg     : std_logic_vector(value_in'range);
     signal req              : std_logic;
 begin
 
-value_wr_r_p: process(clk_from)
-begin
-    if rising_edge(clk_from) then
-        value_wr_r <= value_wr;
-    end if;
-end process value_wr_r_p;
-
-value_wr_one <= value_wr_r and not value_wr;
-
-value_in_reg_p: process(clk_from)
-begin
-    if rising_edge(clk_from) then
-        if value_wr = '1' then
-            value_in_reg <= value_in;
-        end if;
-    end if;
-end process value_in_reg_p;
-
 req_t: entity work.toggle
 port map(
-    toggle_in   => value_wr_one,
+    toggle_in   => value_wr,
     toggle_out  => req,
+    busy        => busy,
     clk_from    => clk_from,
     clk_to      => clk_to
 );
 
-value_out_reg_p: process(clk_to)
+value_out_reg: process(clk_to)
 begin
     if rising_edge(clk_to) then
         if req = '1' then
-            value_out <= value_in_reg;
+            value_out <= value_in;
         end if;
     end if;
-end process value_in_reg_p;
+end process value_out_reg;
 
 end Structural;
 
