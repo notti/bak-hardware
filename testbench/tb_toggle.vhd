82630a6897c6075df91aafcbb1a868a073b2d734
Gernot Vormayr <gvormayr@gmail.com>
2012-07-16 20:05:45 +0200

    tb for toggle

--- testbench/tb_toggle.vhd
+++ testbench/tb_toggle.vhd
@@ -1,72 +1,56 @@
 library IEEE;
         use IEEE.STD_LOGIC_1164.ALL;
-        use IEEE.STD_LOGIC_UNSIGNED.ALL;
-        use IEEE.NUMERIC_STD.ALL;
+        use IEEE.STD_LOGIC_SIGNED.ALL;
+        use IEEE.STD_LOGIC_ARITH.ALL;
 library std;
         use std.textio.all;
-library UNISIM;
-use UNISIM.vcomponents.all;
-
-library proc;
-use proc.all;
+library work;
+use work.all;
 
 entity tb_toggle is
 end tb_toggle;
 
 architecture behav of tb_toggle is
-    signal toggle_in    : std_logic;
-    signal toggle_out   : std_logic;
-    signal clk_from     : std_logic;
-    signal clk_to       : std_logic;
-    signal rst          : std_logic;
+    signal clka     : std_logic := '0';
+    signal clkb     : std_logic := '0';
+    signal toggle_in: std_logic := '0';
+    signal toggle_out: std_logic;
+    signal busy: std_logic;
+
 begin
     
-    clock_from: process
+    clk_a: process
     begin
-        wait for 1 ns;
-        loop
-            clk_from <= '0', '1' after 5 ns;
-            wait for 10 ns;
-        end loop;
-    end process clock_from;
-    clock_to: process
-    begin
-        wait for 3 ns;
-        loop
-            clk_to <= '0', '1' after 5 ns;
-            wait for 10 ns;
-        end loop;
-    end process clock_to;
+        clka <= '1', '0' after 5 ns;
+        wait for 10 ns;
+    end process clk_a;
 
+    clk_b: process
+    begin
+        clkb <= '1', '0' after 6 ns;
+        wait for 12 ns;
+    end process clk_b;
+    
     process
     begin
-        toggle_in <= '0';
-        rst <= '1', '0' after 15 ns;
-        
-        wait for 6 ns;
-
-        wait for 30 ns;
-
-        toggle_in <= '1', '0' after 20 ns;
-
-        wait for 50 ns;
-
-        toggle_in <= '1', '0' after 20 ns;
-        wait for 300 ns;
-        toggle_in <= '1', '0' after 20 ns;
-        wait for 300 ns;
+        wait for 40 ns;
         toggle_in <= '1';
-
+        wait for 10 ns;
+        toggle_in <= '0';
+        wait for 80 ns;
+        toggle_in <= '1';
+        wait for 100 ns;
+        toggle_in <= '0';
         wait;
     end process;
-    
-    toggle_i: entity proc.toggle
-    port map(
-    rst => rst,
-    toggle_in => toggle_in,
-    toggle_out => toggle_out,
-    clk_from => clk_from,
-    clk_to => clk_to
-    );
 
+    
+    toggle_i: entity work.toggle
+      port map(
+        toggle_in => toggle_in,
+        toggle_out => toggle_out,
+        busy => busy,
+        clk_from => clka,
+        clk_to => clkb
+            );
 end behav;
